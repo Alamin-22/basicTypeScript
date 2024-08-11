715078111cb2e586c6dac5fdc06dcf0396fa4438
Alamin-22 <mdalaminmollick@gmail.com>
2024-08-12 00:38:44 +0600

    s

--- myObject.ts
+++ myObject.ts
@@ -4,6 +4,19 @@ const user = {
   isActive: true,
 };
 
-function CreateUser(name: string, email: string, isActive: boolean) {}
+function createUser({ name: string, isPaid: boolean }) {}
 
-CreateUser("md alamin mollik", "xyz@gmail.com", false);
+// this line is giving me an Error Because Email type is not Defined into the function
+// createUser({ name: "Md AL Amin Mollik", isPaid: false, email: "x@y.com" });
+
+// but if we pass unknown property from an extra object it will accept it without any error. This is Type Script Wired behavior
+
+let userInfo = { name: "Md AL Amin Mollik", isPaid: false, email: "x@y.com" };
+
+createUser(userInfo);
+
+function createCourse(): { name: string; price: number } {
+  return { name: "TypeScript", price: 5 };
+}
+
+export {};

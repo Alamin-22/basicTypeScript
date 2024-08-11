90c8d1efd952a33e24d15b6778e38bbfd70c2c58
Alamin-22 <mdalaminmollick@gmail.com>
2024-08-12 00:19:43 +0600

    practcing object

---
+++ myObject.ts
@@ -0,0 +1,9 @@
+const user = {
+  name: "md alamin Mollik",
+  email: "xyz@gmail.com",
+  isActive: true,
+};
+
+function CreateUser(name: string, email: string, isActive: boolean) {}
+
+CreateUser("md alamin mollik", "xyz@gmail.com", false);

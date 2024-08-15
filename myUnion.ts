58f71be2245e3ee93f97026f305ec8a92b057539
Alamin-22 <mdalaminmollick@gmail.com>
2024-08-15 11:34:58 +0600

    .

--- myUnion.ts
+++ myUnion.ts
@@ -16,3 +16,15 @@ type Admin = {
 let alamin: User | Admin = { name: "mollik", id: 51 };
 
 alamin = { username: "mollik", id: 123 };
+
+// in this provided code I have added A condition for the type. either number neither  string.
+function getDBId(id: number | string) {
+  console.log("DB id is = ", id);
+}
+
+getDBId(5); // i can pass number as an arguments
+getDBId("mollik"); // also i can pass string as an arguments
+
+// but if i want to do something extra on with this function then I have to check the type using if else otherwise it might generate some issue because of its type
+
+export {};
